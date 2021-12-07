d97959f844e862e5a8fd2d90f4f3e519fe6f3595
Mansour Marvasti <mansour.marvasti@outlook.com>
2021-12-07 12:24:38 +0330

    use SSD1306 lib

--- aws.ino
+++ aws.ino
@@ -1,3 +1,12 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET 7
+#define OLED_SA0   8
+Adafruit_SSD1306 display(OLED_RESET, OLED_SA0);
+
 #define buzzer_pin 11
 void setup()
 {
