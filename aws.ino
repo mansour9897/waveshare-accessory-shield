3caa57bf812e5195147ea2f7e1bcdbd883466141
Mansour Marvasti <mansour.marvasti@outlook.com>
2021-12-07 12:37:59 +0330

    addd init text on oled

--- aws.ino
+++ aws.ino
@@ -16,6 +16,27 @@ void setup()
     digitalWrite(buzzer_pin,HIGH);
     delay(500);
     digitalWrite(buzzer_pin,LOW);
+
+    // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
+  // init done
+  
+  // Show image buffer on the display hardware.
+  // Since the buffer is intialized with an Adafruit splashscreen
+  // internally, this will display the splashscreen.
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(5,5);
+  display.println("Auther :");
+  display.display();
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(5,25);
+  display.println("M.Marvasti");
+  display.display();
+  delay(2000);
+  display.clearDisplay();
 }
 
 void loop()
